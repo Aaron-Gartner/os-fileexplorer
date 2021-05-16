99d202ab03be2a02bdca8d3b8233a199fbc98dd9
Aaron-Gartner <gart1353@stthomas.edu>
2021-05-15 20:16:08 -0500

    Continued work

--- src/main.cpp
+++ src/main.cpp
@@ -33,8 +33,15 @@ typedef struct AppData {
     TTF_Font *font;
     SDL_Texture *icons[6];
     std::vector<drawItem*> file_list;
+    SDL_Rect scroll_button_up;
+    SDL_Rect scroll_button_down;
+    SDL_Rect TurnRecursiveOn;
     bool icon_selected;
     bool filename_selected;
+    bool scroll_button_down_selected;
+    bool scroll_button_up_selected;
+    bool TurnRecursiveOn_selected;
+    bool recursive_turned_on;
     /*
     bool directory_icon_selected;
     bool executable_icon_selected;
@@ -49,6 +56,7 @@ typedef struct AppData {
     bool other_filename_selected;*/
 } AppData;
 
+void listDirectoryRecursive(std::string dirname, std::vector<drawItem*>& file_list, SDL_Renderer *renderer, AppData *data_ptr, int indent);
 void listDirectoryNon_Rec(std::string dirname, std::vector<drawItem*>& file_list, SDL_Renderer *renderer, AppData *data_ptr);
 void initialize(SDL_Renderer *renderer, AppData *data_ptr);
 void render(SDL_Renderer *renderer, AppData *data_ptr);
@@ -61,7 +69,7 @@ int main(int argc, char **argv)
 
     // initializing SDL as Video
     SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_PNG);+
+    IMG_Init(IMG_INIT_PNG);
     TTF_Init();
     // create window and renderer
     SDL_Renderer *renderer;
@@ -158,12 +166,29 @@ void render(SDL_Renderer *renderer, AppData *data_ptr) {
     // erase renderer content
     SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
     SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 200, 255);
+    data_ptr->scroll_button_up.x = 770;
+    data_ptr->scroll_button_up.y = 0;
+    data_ptr->scroll_button_up.w = 30;
+    data_ptr->scroll_button_up.h = 30;
+    SDL_RenderFillRect(renderer, &(data_ptr->scroll_button_up));
+    data_ptr->scroll_button_down.x = 770;
+    data_ptr->scroll_button_down.y = 570;
+    data_ptr->scroll_button_down.w = 30;
+    data_ptr->scroll_button_down.h = 30;
+    SDL_RenderFillRect(renderer, &(data_ptr->scroll_button_down));
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    data_ptr->TurnRecursiveOn.x = 0;
+    data_ptr->TurnRecursiveOn.y = 540;
+    data_ptr->TurnRecursiveOn.w =60;
+    data_ptr->TurnRecursiveOn.h =60;
+    SDL_RenderFillRect(renderer, &(data_ptr->TurnRecursiveOn));
+
     for(int i = 0; i < data_ptr->file_list.size(); i++) {
         //icon, name, size, value
        SDL_QueryTexture(data_ptr->file_list[i]->file_name_texture, NULL, NULL, &(data_ptr->file_list[i]->file_name_rect.w), &(data_ptr->file_list[i]->file_name_rect.h));
        SDL_RenderCopy(renderer, data_ptr->file_list[i]->file_name_texture, &(data_ptr->file_list[i]->file_name_rect), &(data_ptr->file_list[i]->icon_rect));
     }
-
     // show rendered frame
     SDL_RenderPresent(renderer);
 }
@@ -268,7 +293,108 @@ void listDirectoryNon_Rec(std::string dirname, std::vector<drawItem*>& file_list
                 y += 10;
             }
         }
-        render(renderer, data_ptr);
+        closedir(dir);
+    }
+    else
+    {
+        fprintf(stderr, "Error: directory '%s' not found\n", dirname.c_str());
+    }   
+}
+
+
+
+void listDirectoryRecursive(std::string dirname, std::vector<drawItem*>& file_list, SDL_Renderer *renderer, AppData *data_ptr, int indent = 0) {
+    for (int i = 0; i < file_list.size(); i++) {
+        SDL_DestroyTexture(file_list[i]->file_name_texture);
+    }
+    file_list.clear();
+    std::string space = "";
+    for (int i = 0; i < indent; i++) {
+        space += " ";
+    }
+    struct stat info;
+    int err = stat(dirname.c_str(), &info);
+    if (err == 0 && S_ISDIR(info.st_mode)) {
+        DIR* dir = opendir(dirname.c_str());
+        std::vector<std::string> filenames;
+        struct dirent* entry;
+        while ((entry = readdir(dir)) != NULL) {
+            filenames.push_back(entry->d_name);
+        }
+        std::sort(filenames.begin(), filenames.end());
+        struct stat file_info;
+        //Declared here to ensure items don't render on top of eachother
+        int y = 20;
+        for(int i = 0; i < filenames.size(); i++) {
+            err = stat((dirname + "/" + filenames[i]).c_str(), &file_info);
+            if (err) {
+                fprintf(stderr, "File does not exist");
+            } else {
+                if(S_ISDIR(file_info.st_mode)) {
+                    drawItem *toPush = new drawItem();
+                    //fill in fields toPush
+                    toPush->file_name_rect.x = 0;
+                    toPush->file_name_rect.y = y;
+                    //TODO Question: what to set for file_name_text? 
+                    //Look at video (the portion for hello world)
+                    SDL_Color text_color = { 0, 0, 0 };
+                    toPush->file_name_surface  = TTF_RenderText_Solid(data_ptr->font, filenames[i].c_str(), text_color);
+                    toPush->file_name_texture = SDL_CreateTextureFromSurface(renderer, toPush->file_name_surface);
+                    SDL_FreeSurface(toPush->file_name_surface);
+                    toPush->type = directory_icon; 
+                    toPush->icon_rect.x = 0;
+                    toPush->icon_rect.y = y;
+                    toPush->icon_rect.w = 40;
+                    toPush->icon_rect.h = 30;
+                    file_list.push_back(toPush);
+                    //One line of code to 
+                    if (filenames[i] != "." && filenames[i] != "..") {
+                         listDirectoryRecursive((dirname + "/" + filenames[i]), data_ptr->file_list, renderer, data_ptr, (indent+1));
+                    }       
+                } else {
+                    drawItem *toPush = new drawItem();
+                    SDL_Color text_color = { 0, 0, 0 };
+                    toPush->file_name_surface  = TTF_RenderText_Solid(data_ptr->font, filenames[i].c_str(), text_color);
+                    toPush->file_name_texture = SDL_CreateTextureFromSurface(renderer, toPush->file_name_surface);
+                    SDL_FreeSurface(toPush->file_name_surface);
+                    toPush->file_name_rect.x = 0;
+                    toPush->file_name_rect.y = y;
+                    toPush->file_name_rect.w = 40;
+                    toPush->file_name_rect.h = 30;
+                    toPush->icon_rect.x = 0;
+                    toPush->icon_rect.y = y;
+                    toPush->icon_rect.w = 40;
+                    toPush->icon_rect.h = 30;
+                    for(int i=0; i < file_list.size(); i++){
+                        std::string file_extension = filenames[i].substr(filenames[i].find('.'));
+                        if(file_extension == ".jpg" || file_extension == ".jpeg" || file_extension == ".png" || file_extension == ".tif" 
+                        || file_extension == ".tiff" || file_extension == ".gif") {
+
+                            toPush->type = image_icon;
+
+                        }else if(file_extension == ".mp4" || file_extension == ".mov" || file_extension == ".mkv" || file_extension == ".avi" 
+                        || file_extension == ".webm") {
+
+                            toPush->type = video_icon;
+
+                        }else if(file_extension == ".h" || file_extension == ".c" || file_extension == ".cpp" || file_extension == ".py" 
+                        || file_extension == ".java" || file_extension == ".js") {
+
+                            toPush->type = video_icon;
+
+                        }else {
+
+                            toPush->type = other_icon;
+
+                        }
+
+                    }
+                   
+                    file_list.push_back(toPush);
+                }
+                y += 10;
+            }
+        }
         closedir(dir);
     }
     else
